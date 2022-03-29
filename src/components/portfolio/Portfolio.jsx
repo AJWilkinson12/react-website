ca7881aa106f1a03151c348e8644f6399f7271f9
AJ Wilkinson <ajwilkinson18@gmail.com>
2022-03-29 00:09:07 -0700

    Fixed layout

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/IMG1.png'
+import IMG2 from '../../assets/IMG2.jpeg'
+import IMG3 from '../../assets/IMG3.jpeg'
+import IMG4 from '../../assets/IMG4.jpeg'
 
 const Portfolio = () => {
   return (
@@ -13,36 +16,36 @@ const Portfolio = () => {
           <div className="portfolio__item-image">
             <img src={IMG1} alt="img1"/>
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>A* Search Visualizer</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com/AJWilkinson12/AStar_Algo_Visualizer" className='btn' target="_blank" rel="noreferrer">Github</a>
           </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="img1"/>
+            <img src={IMG2} alt="img1"/>
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>Assuarnce Mobile App (Weapon Detection App)</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com/TechForGoodInc/Assurance-Mobile-UI" className='btn' target="_blank" rel="noreferrer">Github</a>
           </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="img1"/>
+            <img src={IMG3} alt="img1"/>
           </div>
-          <h3>This is a portfolio item title</h3>
+          <h3>Sports betting client</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com/AJWilkinson12/sports-odds-client" className='btn'>Github</a>
           </div>
         </article>
         <article className="portfolio__item">
           <div className="portfolio__item-image">
-            <img src={IMG1} alt="img1"/>
+            <img src={IMG4} alt="img1"/>
           </div>
           <h3>This is a portfolio item title</h3>
           <div className="portfolio__item-cta">
-            <a href="https://github.com" className='btn'>Github</a>
+            <a href="https://github.com/sfbowen4/GoAdventure" className='btn'>Github</a>
           </div>
         </article>
       </div>
