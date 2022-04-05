3f160016620820042387d43f4b914291f5e1adc5
AJ Wilkinson <ajwilkinson18@gmail.com>
2022-04-05 16:16:20 -0500

    Added possible loading screen to allow rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -7,19 +7,29 @@ import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import RingLoader from "react-spinners/RingLoader";
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 5000)
+  }, [])
+
   return (
-    <>
-      <Header/>
-      <Nav/>
-      <About/>
-      <Experience/>
-      <Services/>
-      <Portfolio/>
-      <Contact/>
-      <Footer/>
-    </>
+    <div>
+    {
+      loading ? 
+      <div className='Loader'>
+        <RingLoader color={"#d93d2d"} loading={loading} size={100} />
+      </div>
+      :
+      <><Header /><Nav /><About /><Experience /><Services /><Portfolio /><Contact /><Footer /></>
+    }
+    </div>
   )
 }
 
